47b4a3560e19b5b59551758ba86ab4c11d92c3fb
PAVIN C <124652800+Pavin-C@users.noreply.github.com>
2023-10-02 21:06:18 +0530

    Add files via upload

---
+++ success.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subscription Success</title>
+    <link href="{{ asset('/assets/img/favicon.png') }}" rel="icon">
+</head>
+<body style="font-family: Arial, sans-serif; background-color: #f0f0f0; text-align: center;">
+
+    <div style="background-color: #ffffff; border-radius: 10px; padding: 20px; margin: 20px auto; max-width: 400px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
+        <h1 style="color: #4CAF50;">Subscription Successful!</h1>
+        <p style="font-size: 18px; color: #333;">Thank you for subscribing to our newsletter.</p>
+        <a href="{{url('/')}}" style="font-size: 16px; color: #777;">Back to home</a>
+    </div>
+
+</body>
+</html>
