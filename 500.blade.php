a218da9c6c7af62175f7cc55edd6f8b4f9b28bd9
PAVIN C <124652800+Pavin-C@users.noreply.github.com>
2023-10-02 21:02:58 +0530

    Add files via upload

---
+++ 500.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Internal Server Error</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f1f1f1;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+        .error-container {
+            background-color: #fff;
+            max-width: 500px;
+            margin: 50px auto;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        }
+        h1 {
+            color: #f44336;
+        }
+        p {
+            font-size: 18px;
+            margin: 20px 0;
+        }
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 3px;
+        }
+        .button:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+<body>
+    <div class="error-container">
+        <h1>Internal Server Error</h1>
+        <p>Oops! Something went wrong on our end. We apologize for the inconvenience.</p>
+        <p>Please try again later, or contact our support team for assistance.</p>
+        <a href="/" class="button">Go to Home</a>
+    </div>
+</body>
+</html>
